50e4a220eb8e3b969a203c33db1fcba163160c5a
Dudnik <vladislav_d@ukr.net>
2020-03-03 09:36:40 +0200

    Добавил не включенный проект

--- Client/ReferenceCreator.cs
+++ Client/ReferenceCreator.cs
@@ -2,32 +2,17 @@
 using ConlluObject.Tools.TreeBuilder;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using Trestan;
 
 namespace Client
 {
 	public partial class ReferenceCreator : Form
 	{
-		clsResize _form_resize;
-
 		public ReferenceCreator(List<Sentence> sentences)
 		{
 			InitializeComponent();
 
-			//_form_resize = new clsResize(this);
-			//this.Load += _Load;
-			//this.Resize += _Resize;
-
 			var conlluTreeBuilder = new ConlluTreeBuilder(sentences);
-
-			//var conlluTree = conlluTreeBuilder.BuildTree();
 
 			conlluTreeBuilder.PassConlluTree(treeLeft.Nodes, conlluTreeBuilder.BuildTree());
 			conlluTreeBuilder.PassConlluTree(treeRight.Nodes, conlluTreeBuilder.BuildTree());
@@ -58,16 +43,6 @@ namespace Client
 					mnu.Show(treeLeft, e.Location);
 				}
 			};
-		}
-
-		private void _Load(object sender, EventArgs e)
-		{
-			_form_resize._get_initial_size();
-		}
-
-		private void _Resize(object sender, EventArgs e)
-		{
-			_form_resize._resize();
 		}
 	}
 }
